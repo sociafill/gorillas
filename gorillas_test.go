772ac2676c9bd66d2aef8eb8a8de557c18111c0e
Johnny Debt <38006769+johnny-debt@users.noreply.github.com>
2018-07-25 15:09:07 +0300

    Tests update

--- gorillas_test.go
+++ gorillas_test.go
@@ -1,7 +1,17 @@
 package gorillas
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/gorilla/websocket"
+)
 
 func TestConstructor(t *testing.T) {
 	NewGorillas()
+}
+
+func TestAddConnectionSuccessfully(t *testing.T) {
+	gorillas := NewGorillas()
+	connection := websocket.Conn{}
+	gorillas.AddConnection(&connection)
 }
